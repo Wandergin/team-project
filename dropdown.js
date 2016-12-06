d5964ea3ebaa1de08826b196dc5770e9f95f866b
Duy H Le <2140233l@bo720-3-06u.dcs.gla.ac.uk>
2016-12-06 16:16:08 +0000

    Refined basic suggestion box from Dropdown-complete and added it to main index.html, issue #5

---
+++ dropdown.js
@@ -0,0 +1,8 @@
+$( document ).ready(function() {
+	var input = document.getElementById("query");
+	var awesomplete = new Awesomplete(input);
+
+	/* ...more code to be added... */
+
+	awesomplete.list = ["Indian", "Indian restaurants near me", "Sushi", "Thai food", "Restaurants in city centre", "Chinese restaurants", "Cheap restaurants in Glasgow"];
+});
