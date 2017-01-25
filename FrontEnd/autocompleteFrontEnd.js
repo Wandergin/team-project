7dacc5c0f5b4661193ec551407943f3bb3559b86
MariusVin <m.vinickis@gmail.com>
2017-01-25 19:03:31 +0000

    Working on suggestion list 2 #19

--- FrontEnd/autocompleteFrontEnd.js
+++ FrontEnd/autocompleteFrontEnd.js
@@ -5,99 +5,109 @@ var locations = [
         "Dundee"
     ];
 
-var times = []
+//First suggested category will be cuisine
+
+var linkedList;
+
+var times = [];
 
 var terms = [];
 
-var response = '{"date": "", "cuisine": ["chinese"], "location": [], "covers": "", "time": ""}';
+var response = '{"date": "", "cuisine": [], "location": [], "covers": "", "time": ""}';
 
-function sendDataToServer(data) {
-    $.ajax({
+function sendDataToServer(data, linkedList, suggestCat) {
+    console.log(suggestCat);
+    var response = $.ajax({
         url:"http://localhost:5000/search",
         method:"GET",
         data:{"q":data},
         success:function(res) {
-
-            console.log("Successfully sent data.");
-            console.log(res);
-
-            var response = JSON.parse(res);
-
-            //Check if all categories (keys) have associated data
-            //If yes, set data, mark current node as complete
-            //If no, remove data, mark as incomplete
-            //Will be incomplete by default but change is necessary when terms are deleted
-
-            if (res.cuisine != ""){
-                (linkedList.searchNodeType('cuisine')).setData(response.cuisine);
-                (linkedList.searchNodeType('cuisine')).markComplete();
-            } else {
-                (linkedList.searchNodeType('cuisine')).setData("");
-                (linkedList.searchNodeType('cuisine')).markNotComplete();
-            }
-
-            if (res.location != ""){
-                (linkedList.searchNodeType('location')).setData(response.location);
-                (linkedList.searchNodeType('location')).markComplete();
-            } else {
-                (linkedList.searchNodeType('location')).setData("");
-                (linkedList.searchNodeType('location')).markNotComplete();
-            }
-
-            if (res.rating != ""){
-                (linkedList.searchNodeType('rating')).setData(response.rating);
-                (linkedList.searchNodeType('rating')).markComplete();
-            } else {
-                (linkedList.searchNodeType('rating')).setData("");
-                (linkedList.searchNodeType('rating')).markNotComplete();
-            }
-
-            if (res.people != ""){
-                (linkedList.searchNodeType('people')).setData(response.people);
-                (linkedList.searchNodeType('people')).markComplete();
-            } else {
-                (linkedList.searchNodeType('people')).setData("");
-                (linkedList.searchNodeType('people')).markNotComplete();
-            }
-
-            if (res.time != ""){
-                (linkedList.searchNodeType('time')).setData(response.time);
-                (linkedList.searchNodeType('time')).markComplete();
-            } else {
-                (linkedList.searchNodeType('time')).setData("");
-                (linkedList.searchNodeType('time')).markNotComplete();
-            }
-
-            //See which nodes are marked as incomplete
-            //Set first incomplete node found as next in-box suggestion category
-            for (var i = 0; i <= linkedList._length; i++){
-                if (suggestCat.completed == true){
-                    suggestCat = suggestCat.next;
-                //i == linkedList._length only if all nodes are marked complete
-                //Set suggestCat as null, nothing more to suggest
-                } else if (i == linkedList._length){
-                    suggestCat = null;
-                }
-            }
-
-        }
+                    }
     });
+
+    console.log("Successfully sent data.");
+    console.log(response)
+    //response = JSON.parse(response);
+
+    //Check if all categories (keys) have associated data
+    //If yes, set data, mark current node as complete
+    //If no, remove data, mark as incomplete
+    //Will be incomplete by default but change is necessary when terms are deleted
+
+    if (response.cuisine != ""){
+        (linkedList.searchNodeType('cuisine')).setData(response.cuisine);
+        (linkedList.searchNodeType('cuisine')).markComplete();
+    } else {
+        (linkedList.searchNodeType('cuisine')).setData("");
+        (linkedList.searchNodeType('cuisine')).markNotComplete();
+    }
+
+    if (response.location != ""){
+        (linkedList.searchNodeType('location')).setData(response.location);
+        (linkedList.searchNodeType('location')).markComplete();
+    } else {
+        (linkedList.searchNodeType('location')).setData("");
+        (linkedList.searchNodeType('location')).markNotComplete();
+    }
+
+    if (response.rating != ""){
+        (linkedList.searchNodeType('rating')).setData(response.rating);
+        (linkedList.searchNodeType('rating')).markComplete();
+    } else {
+        (linkedList.searchNodeType('rating')).setData("");
+        (linkedList.searchNodeType('rating')).markNotComplete();
+    }
+
+    if (response.people != ""){
+        (linkedList.searchNodeType('people')).setData(response.people);
+        (linkedList.searchNodeType('people')).markComplete();
+    } else {
+        (linkedList.searchNodeType('people')).setData("");
+        (linkedList.searchNodeType('people')).markNotComplete();
+    }
+
+    if (response.time != ""){
+        (linkedList.searchNodeType('time')).setData(response.time);
+        (linkedList.searchNodeType('time')).markComplete();
+    } else {
+        (linkedList.searchNodeType('time')).setData("");
+        (linkedList.searchNodeType('time')).markNotComplete();
+    }
+
+    //See which nodes are marked as incomplete
+    //Set first incomplete node found as next in-box suggestion category
+    for (var i = 0; i <= linkedList._length; i++){
+        if (suggestCat.completed == true){
+            suggestCat = suggestCat.next;
+        //i == linkedList._length only if all nodes are marked complete
+        //Set suggestCat as null, nothing more to suggest
+        } else if (i == linkedList._length){
+            suggestCat = null;
+        }
+    }
+    updateSuggestionList(suggestCat, data);
+
+
+    console.log(suggestCat);
+    return suggestCat;
 }
 
-function updateSuggestionList(relevantList, previousQuery) {
-    var suggestionList = [];
-    for (key in relevantList) {
-        suggestionList.push(previousQuery + " " + locations[key]);
-    }
-    jQuery('#input').autocomplete('destroy');
-    $("#input").autocomplete({
-        source:[suggestionList]
-    });
+function updateSuggestionList(suggestedCategory, previousQuery) {
+    console.log(suggestedCategory);
+    // var suggestionList = [];
+    // for (key in relevantList) {
+    //     suggestionList.push(previousQuery + " " + locations[key]);
+    // }
+    // jQuery('#input').autocomplete('destroy');
+    // $("#input").autocomplete({
+    //     source:[suggestionList]
+    // });
 }
 
 $( document ).ready(function() {
     //Construct the linkedList for suggested categories to appear in query bar
     var linkedList = new LinkedList();
+    var suggestCat = linkedList.head;    
 
 
     linkedList.add(1, 'cuisine');
@@ -106,8 +116,6 @@ $( document ).ready(function() {
     linkedList.add(4, 'people');
     linkedList.add(5, 'time');
 
-    //First suggested category will be cuisine
-    var suggestCat = (linkedList.head);
 
     var queryText = $("#input").val();
 
@@ -120,7 +128,7 @@ $( document ).ready(function() {
         console.log(e.which);
         if ((e.which > 47) && (e.which < 111)){
             console.log("Yay")
-            sendDataToServer($("#input").val());
+            sendDataToServer($("#input").val(), suggestCat, linkedList);
         }
         else if (e.which == 8) {
             console.log("backspace");
