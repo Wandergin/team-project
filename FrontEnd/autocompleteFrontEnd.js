04d6c2f7c39eb611ecd35d21a764db941557a242
MariusVin <m.vinickis@gmail.com>
2017-01-25 15:25:44 +0000

    Added listeners and started working on linkedList #19

--- FrontEnd/autocompleteFrontEnd.js
+++ FrontEnd/autocompleteFrontEnd.js
@@ -1,4 +1,4 @@
-    var terms = [ 
+var terms = [ 
         "table",
         "table for 2 people", 
         "table for 2 people in Glasgow",
@@ -9,10 +9,50 @@
         "Scottish cousine Glasgow 2 people",
         "Scottish cousine Glasgow 2 people 8pm"
     ];
-    $(function() {
 
-        $("#input")
-            .autocomplete({
-                source:[terms]
-            });
+
+function sendDataToServer(data) {
+    $.ajax({
+        url:"localhost:8080/search",
+        method:"GET",
+        data:{data:data},
+        success:function(res) {
+            console.log("Successfully sent data.");
+            console.log(res);
+        }
     });
+}
+
+$( document ).ready(function() {
+    var linkedList = new LinkedList;
+    linkedList.add(terms[0]);
+    console.log(linkedList);
+
+    // listening to keypress
+    $(document).keyup(function(e) {
+        console.log(e);
+        switch(e.which){
+            case 8:
+                console.log("backspace");
+                break; 
+            
+            case 46:
+                console.log("delete");
+                break;
+            
+            case 39:
+                console.log("User autocompleted the term.");
+                break;
+            
+            default:
+                console.log($("#input").val());
+                break;
+        }
+    });
+
+    // Initialize autocomplete
+    $("#input").autocomplete({
+            source:[terms]
+        });
+
+});
