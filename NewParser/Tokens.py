71e531afcca1107343c7feb324e2e15e98b00d6e
Jamie <321.jamiesweeney.321@gmail.com>
2017-02-19 21:14:25 +0000

    Fixes spelling errors in Tokens.py

--- NewParser/Tokens.py
+++ NewParser/Tokens.py
@@ -34,8 +34,8 @@ Dates_common = 		["today", "tomorrow"]
 
 Dates_days =		{"monday": 1, "tuesday": 2, "wednesday": 3, "thursday": 4, "friday": 5, "saturday": 6, "sunday": 7}
 
-Dates_months = 		["january": 1, "february": 2, "march": 3, "april": 4, "may": 5, "june": 6, "july": 7, "august": 8,
-			"september": 9, "october": 10, "november": 11, "december": 12]
+Dates_months = 		{"january": 1, "february": 2, "march": 3, "april": 4, "may": 5, "june": 6, "july": 7, "august": 8,
+			"september": 9, "october": 10, "november": 11, "december": 12}
 
 
 ##############################
