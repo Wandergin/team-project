af2113def3d2ac118fe6ba61d6f17e5632bf6eab
MariusVin <m.vinickis@gmail.com>
2017-02-20 13:39:11 +0000

    Started working on tokenizable search #47

---
+++ FrontEnd/tokenizedQuery/init.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+
+   $('#input').selectize({
+        plugins: ['remove_button'],
+        delimiter: ',',
+        persist: false,
+        create: function(input) {
+            return {
+                value: input,
+                text: input
+            }
+        }
+    }); 
+});
