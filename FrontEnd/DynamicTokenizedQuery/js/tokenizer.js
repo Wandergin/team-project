e2604adf035aa511ef39776d8ee281b2630661da
thatWanderer <m.vinickis@gmail.com>
2017-03-06 14:39:34 +0000

    Fixed the styling of the tokens, moving on to query processing #53 #23

--- FrontEnd/DynamicTokenizedQuery/js/tokenizer.js
+++ FrontEnd/DynamicTokenizedQuery/js/tokenizer.js
@@ -1,16 +1,42 @@
 tokens = ["Chinese","table for 2", "at 9pm"];
+quert = "";
+
+function forcePlaceholderRemoval() {
+    var empty = true;
+    if ($(".items").children().length > 2) {
+        empty = false
+    }
+    if (empty == false) {
+        $("#input-tags").attr("placeholder","");
+    }
+    else {
+        $("#input-tags").attr("placeholder","City, postcode or restaurant name");
+    }
+}
+
+function removeButton(o) {
+    o.parent().remove();
+    forcePlaceholderRemoval();
+}
 
 function crawlAndCollect(items) {
+    query = "";
+    var inputQuery = "";
     // Go through each of the items, if it's a div, extract it's token, if it's an
     // input field, extract it's value
+    console.log("Crawling...");
     items.children().each(function(){
-        console.log("Hi")
+        console.log($(this));
+        if ($(this)[0].nodeName != "BUTTON") {
+            query += $(this)[0].innerText;
+        }
     });
-    
-    var value = items.children(":text")[0].value;
-    console.log(value);
-    
-    return value;
+
+    query += items.children(":text")[0].value;
+    console.log("QUERY: "+query);
+
+    inputQuery = items.children(":text")[0].value
+    return inputQuery;
 }
 
 $(document).ready(function(){
@@ -25,30 +51,42 @@ $(document).ready(function(){
             e.preventDefault();
             console.log("delete");
         }
+
         console.log($(".items"));
-        var query = crawlAndCollect($(".items"));
+        var inputQuery = crawlAndCollect($(".items"));
         //console.log(query);
         $.each(tokens, function(index, token){
-            if (query.indexOf(token) >= 0) {
+            if (inputQuery.indexOf(token) >= 0) {
                 console.log("Found " + token);
-                caretStart = query.indexOf(token)
+                caretStart = inputQuery.indexOf(token)
                 var caretEnd = caretStart + token.length;
                 console.log("The token is between "+ caretStart +" and " + caretEnd);
-                query = query.substring(caretEnd,query.length);
-                console.log("Query should be " + query);
-                var $div = $('<div class="item" data-token="cuisine" data-value="chinese">'+token+'<a href="javascript:void(0)" class="remove" tabindex="-1" title="Remove">×</a></div>');
-               
+                inputQuery = inputQuery.substring(caretEnd,query.length);
+                console.log("Query should be " + inputQuery);
+                var $div = $('<div class="item" data-token="cuisine" data-value="chinese">'+token+'<a href="javascript:void(0)" onClick="removeButton($(this))" class="remove" tabindex="-1" title="Remove">×</a></div>');
+                
+
 
                 // Query constructor
                 var rest = $("#input-tags").clone();
                 // $(".items").remove($("#input-tags"));
                 $("#input-tags").before($div);
-                $("#input-tags").val(query);
+                $("#input-tags").val(inputQuery);
 
                 console.log(token);
                 // console.log(rest);
             }
         });
+        
+        forcePlaceholderRemoval();
+
+    });
+
+    // Click listeners
+    $(".items").click(function(){
+        $("#input-tags").focus();
+
+        // $(this).parent().remove();
     });
 });
 
