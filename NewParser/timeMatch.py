7fdb2066af00c8424ab6e1de6662c2d0440d708d
Jamie <321.jamiesweeney.321@gmail.com>
2017-03-08 22:08:44 +0000

    Fixed bugs and improved time recognisation functionality #41 #42

--- NewParser/timeMatch.py
+++ NewParser/timeMatch.py
@@ -7,6 +7,15 @@ def timeMatch(s):
 	s = s.split(" ")
 	i = 0
 	#Scan for number
+	while i < len(s):
+		if str.isdigit(s[i][0]) and (s[i].endswith("am") or s[i].endswith("pm")):
+			tempStr = s[i]
+			s[i] = tempStr[:-2]
+			s.insert( i+1, tempStr[-2:])
+			break
+		i = i +1
+	i = 0
+
 	while (i < len(s)):
 		if s[i] in str(Tokens.Times_numbers.values()):
 			hour = int(s[i])
@@ -14,8 +23,13 @@ def timeMatch(s):
 		elif Tokens.Times_numbers.has_key(s[i]):
 			hour = int(Times_numbers.get(s[i]))
 			break
+		elif Tokens.Times_approx_meal.has_key(s[i]):
+			return Tokens.Times_approx_meal.get(s[i])
+		elif Tokens.Times_approx.has_key(s[i]):
+			return Tokens.Times_approx.get(s[i])
 		else:
 			i= i+1
+		
 
 	#Scan for AM / PM
 	if i +1 >= len(s):
