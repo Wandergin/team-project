57cb8ebaf21f3deb8e33bf775fd5a4ed517faaad
Jamie <321.jamiesweeney.321@gmail.com>
2017-03-08 22:02:53 +0000

    Added more functionality to recognize times and to estimate #41 #42

--- NewParser/dateProbability.py
+++ NewParser/dateProbability.py
@@ -1,62 +1,97 @@
 import string
 import Tokens
-import dateFinder
 
-def dateProbability(s):
+finalReturn = {}
 
+
+def timeProbability(s):
+
+	#Covert string to lowercase list	
+	s = s.lower()
+	s.translate(None, string.punctuation)
 	s = s.split(" ")
-	i = 0
+	i = len(s) -1
 
-	#For all words in string
-	while (i<len(s)):
-		
-		#Check for today/tomorrow/monday
-		if (s[i] in Tokens.Dates_common) or (s[i] in Tokens.Dates_days) :
-			return 100;
+	returnDict = {}
 
-		#Else check if the first digit is a number (possible date)
-		elif (str.isdigit(s[i][0])):
-			
-			#If date exists return 100:
-			if dateFinder.isDate(s[i]) != "0":
-				return 100
-			
-			#If not might be a date like "2nd of January"		
-			
-			date = s[i]
-			#First word nust be a number (1-31)  followed by month
-			if len(date) > 1 and str.isdigit(date[1]):
-				day = date[1]
-			else:
-				day = date[0]
-				
-			#If day is a real day
-			if int(day) > 0 and int(day) <= 31:
+	#For each word in string
+	while (i >= 0):
 
-				#Check if followed by of
-				i = i + 1
-				if i < len(s) and s[i] == "of":
-					i = i +1
+		#Bool for presence of prefix, number and suffix
+		present_suf = False
+		present_num = False
+		present_pre = False
+		present_approx = False
 
-				#Check if followed by a month
-				if i < len(s) and Tokens.Dates_months.has_key(s[i]):
-					return 100
-			
-		#Not a digit, move on
-		i = i +1
-	
-	#Nothing returned, no date present
-	return 0
+		#Total string with probability
+		result = ""
+		resultProb = 0
 
-print(dateProbability("today"))				#100
-print(dateProbability("tomorrow"))		#100
-print(dateProbability("never"))				#0
-print(dateProbability("ever"))				#0
-print(dateProbability("1.11.17"))			#100	
-print(dateProbability("2nd of january"))	#100
-print(dateProbability("11/1/2017"))		#100
-print(dateProbability("3rd of october"))	#100
-print(dateProbability("32/1/17"))			#0
-print(dateProbability("1.1.2016"))			#0
-print(dateProbability("monday"))			#100
-print(dateProbability("5.15.17")	)			#0
+
+		tempWord = s[i]
+
+		#Search for a suffix that guarantees a time
+		for ending in Tokens.Times_specific_suffixes:
+			#If found take ending out and add to the result
+			if tempWord.endswith(ending):
+				result  = result + ending
+				tempWord = tempWord.replace(ending, '')
+				present_suf = True
+				break
+
+		#If word is empty move to next word
+		if tempWord ==  "" and i > 0:
+			tempWord = s[i-1]
+			result = " " + result  
+			i = i-1
+
+		#Search for a number to match the ending
+		if Tokens.Times_numbers.has_key(tempWord) or tempWord in str(Tokens.Times_numbers.values()):
+			result = tempWord  + result
+			present_num = True
+			i = i-1
+		else:
+			result = ""
+
+		#Search for prefixes to append
+		if i >= 0 and result != "":
+			for prefix in Tokens.Times_specific_prefixes:		#at / for / around
+				if s[i] == prefix:
+					i = i -1
+					result = prefix + " " + result
+					present_pre = True
+					break
+
+		for time in Tokens.Times_approx_meal:				#dinner / lunch
+			if i >= 0 and s[i] == time:
+				i = i-1
+				returnDict.update({time:25})
+				present_approx = True
+				break
+
+		for time in Tokens.Times_approx:				#early late evening
+			if i >= 0 and s[i] == time:
+				i = i-1
+				result = time + " " + result
+				present_approx = True
+				break
+
+
+		#Determines how likely the string is to be a time
+		if present_suf and present_pre and present_num:
+			resultProb = 100
+		elif present_suf and present_num:
+			resultProb = 100
+		elif present_approx and present_pre and present_num:
+			resultProb = 50
+		elif present_pre and present_num:
+			resultProb = 25
+		elif present_approx:
+			resultProb = 25
+
+		if result != "":
+			result = result.strip()
+			returnDict[result] = resultProb
+		else:
+			i = i -1
+	return returnDict
