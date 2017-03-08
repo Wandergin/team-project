d6d8c3ad87e3c12064f7a1f4f0b770bc2b980082
Jamie <321.jamiesweeney.321@gmail.com>
2017-03-08 22:07:32 +0000

    Fixed some bugs and made people no. tokens more flexible #42

--- NewParser/partyProbability.py
+++ NewParser/partyProbability.py
@@ -10,32 +10,33 @@ def partyProbability(s):
 	probability = 0
 
 	while(i< len(s)):
+		result = ""
+		probability = 0
 		if s[i] == "for":
-			result = "for"
+			result = "for "
 			if i > 0:
 				if s[i-1] == "table":
 					result = "table " + result
 					probability += 25
 			if i < len(s):
 				i+=1
-			if s[i] .isdigit():
-				result = result + " " + str(s[i])
-			        probability += 50
-				if i + 1 < len(s):
-					i+=1
-				if s[i] == "people":
-					result = result + " " + "people"
-					probability = 100
-					print result
+		if s[i] .isdigit():
+			result = result + str(s[i])
+		        probability += 50
+			if i + 1 < len(s):
+				i+=1
+			if s[i] == "people":
+				result = result + " " + "people"
+				probability = 100
 
-			elif s[i] in Tokens.People_approx_actual:
+		elif s[i] in Tokens.People_approx_actual:
+			result = result + " " + s[i]
+			probability += 25
+			if i + 1 < len(s):
+				i+=1
+			if s[i] in Tokens.People_approx_suffix:
 				result = result + " " + s[i]
-				probability += 25
-				if i + 1 < len(s):
-					i+=1
-				if s[i] in Tokens.People_approx_suffix:
-					result = result + " " + s[i]
-					probability = 100
-			returnDict[result] = probability
+				probability = 100
+		returnDict[result] = probability
 		i +=1
-	return probability
+	return returnDict
