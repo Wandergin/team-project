380ffddf313ed3f013de3b28df09471e1d750b32
Jamie <321.jamiesweeney.321@gmail.com>
2017-03-08 21:58:53 +0000

    Updated CuisineProbability.py can now recognise multiple word tokens #42

--- NewParser/cuisineProbability.py
+++ NewParser/cuisineProbability.py
@@ -3,24 +3,45 @@ import Tokens
 
 def cuisineProbability(s):
 	s = s.split(" ")	
+	returnArray = []
+	i = 0
 	
-	#Search all ethnic cusines
-	for token in Tokens.Cuisines_ethnic:
-		token = token.split(" ")
-		if s == token:
-			return 1
+	while i < len(s):
+		#Search all ethnic cusines
+		for token in Tokens.Cuisines_ethnic:
+			token = token.split(" ")
+			if compare(s, i, token):
+				returnArray += token
 	
-	#Search all dietary cuisines
-	for token in Tokens.Cuisines_dietary:
-		token = token.split(" ")
-		if s == token:
-			return 1
+		#Search all dietary cuisines
+		for token in Tokens.Cuisines_dietary:
+			token = token.split(" ")
+			if compare(s, i, token):
+				returnArray += token
 
-	#Search all belief cuisines
-	for token in Tokens.Cuisines_belief:
-		token = token.split(" ")
-		if s == token:
-			return 1
+		#Search all belief cuisines
+		for token in Tokens.Cuisines_belief:
+			token = token.split(" ")
+			if compare(s, i, token):
+				returnArray += token
+		
+		i = i +1
 	
-	#If none return 0
-	return 0
+	#Return cuisines
+	if returnArray == []:
+		return []
+	return returnArray
+
+
+
+def compare(s, i, token):
+	if type(token) is str:
+		token = [token]
+
+	if i + len(token) <= len(s):
+		j = 0
+		while j < len(token):
+			if token[j] != s[i+j]:
+				return False
+			j = j + 1
+		return True
