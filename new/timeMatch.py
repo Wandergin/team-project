41eaaaca84ededb22acdfa486f13466a96da6571
cammy <2136555h@student.gla.ac.uk>
2017-03-20 16:44:37 +0000

    41: now supports minutes after hour

--- new/timeMatch.py
+++ new/timeMatch.py
@@ -18,38 +18,61 @@ def timeMatch(s):
 	i = 0
 
 	while (i < len(s)):
-		if s[i] in str(Tokens.Times_numbers.values()):
+		# Formatted time
+		if ":" in s[i]:
+			timeNums = s[i].split(":")
+			print "We might have a proper time"
+			if timeNums[i] in str(Tokens.Times_numbers.values()):
+				print "Found a real num"
+				hour = int(timeNums[0])
+				minute = int(timeNums[1])
+				break
+		elif s[i] in str(Tokens.Times_numbers.values()):
 			hour = int(s[i])
+			print "Direct hour"
+			#if ":" in s[i]:
+				# we might have a minute
+			#	minute = s[i].split(":")[1]
 			break
 		elif Tokens.Times_numbers.has_key(s[i]):
 			hour = int(Tokens.Times_numbers.get(s[i]))
+			print "Hour in Time_numbers"
+		#	if ":" in s[i]:
+				# we might have a minute
+			#	minute = s[i].split(":")[1]
 			break
 		elif Tokens.Times_approx_meal.has_key(s[i]):
+			print "Hour in Times_approx_meal"
 			return Tokens.Times_approx_meal.get(s[i])
 		elif Tokens.Times_approx.has_key(s[i]):
+			print "Hour in Times_approx"
+
 			return Tokens.Times_approx.get(s[i])
 		else:
+			print "incremented"
 			i= i+1
-		
+
+
+
 
 	print hour
 	#Scan for AM / PM
 	if i +1 >= len(s):
-                hour = guessHour(hour) 
+                hour = guessHour(hour)
  	elif s[i+1] == "oclock" or s[i+1] == "o'clock":
-		hour = guessHour(hour) 
+		hour = guessHour(hour)
 	elif s[i+1] == "pm":
 		hour = hour +12
 	elif s[i+1] == "am":
 		hour = hour
 	else:
 		hour = guessHour(hour)
-	
-	answer = str(hour) + ":" + minute
+
+	answer = str(hour) + ":" + str(minute)
 	return answer
 
-	 
-#guessHour: Takes a string and tries to determine if am/pm		
+
+#guessHour: Takes a string and tries to determine if am/pm
 def guessHour(hour):
 	#If in 24h format
 	if 12 < hour < 24:
