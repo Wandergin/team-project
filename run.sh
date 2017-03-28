d61684067534ec1febfc9783e032f531109a31d9
thatWanderer <m.vinickis@gmail.com>
2017-03-28 16:54:03 +0100

    Fixing the filename for logs

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 nohup python ./new/flask_server.py > "Logs/frontEndLog.txt" &
-nohup python FrontEnd/ResdiaryStyle/server.py > "Logs/frontEndLog.txt" &
+nohup python FrontEnd/ResdiaryStyle/server.py > "Logs/backEndLog.txt" &
 
 echo "Usage: localhost:8082/FrontEnd/ResdiaryStyle"
 pgrep -f Python | awk '{print "PID: " $1}'
