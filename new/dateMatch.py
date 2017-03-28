33bdc231a01b0e291fc7fb88f6b0eac8ac343872
cammy <2136555h@student.gla.ac.uk>
2017-03-28 18:49:07 +0100

    #56 fixed issue where date was being returned with a different format depending on input format

--- new/dateMatch.py
+++ new/dateMatch.py
@@ -42,72 +42,72 @@ def addDays(n):
 
 
 def dateMatch(s):
-	s = s.split(" ")
-	i = 0
+    s = s.lower()
+    s = s.split(" ")
+    i = 0
 
+    # For all words in string
+    while (i < len(s)):
 
-	# For all words in string
-	while (i < len(s)):
+        # Check for today/tomorrow
+        if s[i] == "today" or s[i] == "tonight":
+            return time.strftime("%d/%m/%y")
+        elif s[i] == "tomorrow":
+            return addDays(1)
 
-	    # Check for today/tomorrow
-	    if s[i] == "today" or s[i] == "tonight":
-	        return time.strftime("%d/%m/%y")
-	    elif s[i] == "tomorrow":
-	        return addDays(1)
+        # Else check for a day like "monday"
+        if Tokens.Dates_days.has_key(s[i]):
 
-	    # Else check for a day like "monday"
-	    if Tokens.Dates_days.has_key(s[i]):
+            # Calculate day difference
+            dayDiff = Tokens.Dates_days.get(
+                s[i]) - Tokens.Dates_days.get(time.strftime("%A").lower())
+            if dayDiff < 0:
+                dayDiff = dayDiff + 7
 
-	        # Calculate day difference
-	        dayDiff = Tokens.Dates_days.get(
-	            s[i]) - Tokens.Dates_days.get(time.strftime("%A").lower())
-	        if dayDiff < 0:
-	            dayDiff = dayDiff + 7
+            # Look for this / next / x weeks
+            if i > 0:
+                if s[i - 1] == "this":
+                    return addDays(dayDiff)
+                elif s[i - 1] == "next":
+                    return addDays(dayDiff + 7)
+                else:
+                    if i > 2 and s[i - 1] == "on" and (s[i - 2] == "weeks" or s[i - 2] == "week") and str.isdigit(s[i - 3]):
+                        weeksAway = int(s[i - 3])
+                        return addDays(dayDiff + 7 * weeksAway)
 
-	        # Look for this / next / x weeks
-	        if i > 0:
-	            if s[i - 1] == "this":
-	                return addDays(dayDiff)
-	            elif s[i - 1] == "next":
-	                return addDays(dayDiff + 7)
-	            else:
-	                if i > 2 and s[i - 1] == "on" and (s[i - 2] == "weeks" or s[i - 2] == "week") and str.isdigit(s[i - 3]):
-	                    weeksAway = int(s[i - 3])
-	                    return addDays(dayDiff + 7 * weeksAway)
+            # If no next/this then default to this week
+            return addDays(dayDiff)
 
-	        # If no next/this then default to this week
-	        return addDays(dayDiff)
+        # If no day like monday look a number to mean a specific date
+        if (str.isdigit(s[i][0])):
+            # Check for DD/MM/YY
+            date = dateFinder.isDate(s[i])
+            if date != "0":
+                return date
 
-	    # If no day like monday look a number to mean a specific date
-	    if (str.isdigit(s[i][0])):
-	        # Check for DD/MM/YY
-	        date = dateFinder.isDate(s[i])
-	        if date != "0":
-	            return date
+            # Might be a date like "2nd of January"
+            date = s[i]
 
-	        # Might be a date like "2nd of January"
-	        date = s[i]
+            # First word nust be a number (1-31)  followed by month
+            if len(date) > 1 and str.isdigit(date[1]):
+                day = date[:1]
+            else:
+                day = date[0]
 
-	        # First word nust be a number (1-31)  followed by month
-	        if len(date) > 1 and str.isdigit(date[1]):
-	            day = date[:1]
-	        else:
-	            day = date[0]
+            # If day is a real day
+            if int(day) > 0 and int(day) <= 31:
 
-	        # If day is a real day
-	        if int(day) > 0 and int(day) <= 31:
+                # Check if followed by of
+                i = i + 1
+                if i < len(s) and s[i] == "of":
+                    i = i + 1
 
-	            # Check if followed by of
-	            i = i + 1
-	            if i < len(s) and s[i] == "of":
-	                i = i + 1
+                # Check if followed by a month
+                if i < len(s) and Tokens.Dates_months.has_key(s[i]):
 
-	            # Check if followed by a month
-	            if i < len(s) and Tokens.Dates_months.has_key(s[i]):
-
-	                # If it is return the date
-	                if len(day) == 1:
-	                    day = "0" + day
-	                return day + "." + Tokens.Dates_months.get(s[i]) + "." + time.strftime("%y") + "."
-	    i = i + 1
-	return 0
+                    # If it is return the date
+                    if len(day) == 1:
+                        day = "0" + day
+                    return day + "/" + Tokens.Dates_months.get(s[i]) + "/" + time.strftime("%y") + "/"
+        i = i + 1
+    return 0
